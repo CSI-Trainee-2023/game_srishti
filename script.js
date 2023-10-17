17c7cb19dc99e3296928ce8e943cec4ed92166cd
srishtik06 <srishtikaura06@gmail.com>
2023-10-17 20:46:55 +0530

    road animation added

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("start").addEventListener("click",function(){
+    document.getElementById("start").style.display="none"
+    document.getElementById("road").style.animation="roadAnimation 20s linear infinite"
+})
