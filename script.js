df6257e92ecbf16db17801a2c4fec8cdb7d8a30c
srishtik06 <srishtikaura06@gmail.com>
2023-10-18 12:45:26 +0530

    set boundaries for mycar

--- script.js
+++ script.js
@@ -6,18 +6,22 @@ document.getElementById("start").addEventListener("click",function(){
     document.getElementById("obstacle3").style.animation="obs3 4s linear infinite"
     let t=30;
     let l=1.6;
+    const minTop = -40;   // Minimum top (in vh) to prevent moving too far up.
+    const maxTop = 35;   // Maximum top (in vh) to prevent moving too far down.
+    const minLeft = -10;   // Minimum left (in vw) to prevent moving too far left.
+    const maxLeft = 13;
     
     window.addEventListener("keydown", function (x) {
-        if (x.key == "w") {
+        if (x.key == "w" && t> minTop) {
             t=t-5;
         };
-        if (x.key == "a") {
+        if (x.key == "a" && l> minLeft) {
             l=l-2;
         };
-        if (x.key == "s") {
+        if (x.key == "s" &&  t<maxTop) {
             t=t+5;
         };
-        if (x.key == "d") {
+        if (x.key == "d" && l< maxLeft) {
             l=l+2;
         };
         document.getElementById("myCar").style.top=`${t}vh`;
@@ -38,12 +42,18 @@ document.getElementById("start").addEventListener("click",function(){
         num=Math.floor(Math.random()*(164-85+1)+85)
         document.getElementById("obstacle3").style.left=`${num}px`
     }, 4000);
-
+    let highScore= localStorage.getItem("highScore") || 0;
     n = 0
         setInterval(() => {
 
             document.getElementById("score").innerText = `Score : ${n}`
             n = n + 1;
+            document.getElementById("highScore").innerText=`High Score : ${highScore}`;
+            if(n>=highScore) {
+                highScore=n-1;
+                localStorage.setItem("highScore",highScore);
+                document.getElementById("highScore").innerText=`High Score: ${highScore}`;
+            }
 
             var obs1Left = Math.abs(document.getElementById("obs1Img").getBoundingClientRect().left);
             var obs1Right = Math.abs(document.getElementById("obs1Img").getBoundingClientRect().right);
@@ -65,7 +75,7 @@ document.getElementById("start").addEventListener("click",function(){
             var mycarTop = Math.abs(document.getElementById("myCarImg").getBoundingClientRect().top);
             var mycarBottom = Math.abs(document.getElementById("myCarImg").getBoundingClientRect().bottom);
 
-            if (mycarLeft < 265 || mycarRight > 1060 || mycarTop < 20 || mycarBottom > 690 ||
+            if (mycarLeft < 165 || mycarRight > 1060 || mycarTop < 20 || mycarBottom > 690 ||
                 ((obs1Left < mycarLeft && mycarLeft < obs1Right) || (obs1Left < mycarRight && mycarRight < obs1Right)) && ((obs1Top < mycarTop && mycarTop < obs1Bottom) || (obs1Top < mycarBottom && mycarBottom < obs1Bottom)) ||
                 ((obs2Left < mycarLeft && mycarLeft < obs2Right) || (obs2Left < mycarRight && mycarRight < obs2Right)) && ((obs2Top < mycarTop && mycarTop < obs2Bottom) || (obs2Top < mycarBottom && mycarBottom < obs2Bottom)) ||
                 ((obs3Left < mycarLeft && mycarLeft < obs3Right) || (obs3Left < mycarRight && mycarRight < obs3Right)) && ((obs3Top < mycarTop && mycarTop < obs3Bottom) || (obs3Top < mycarBottom && mycarBottom < obs3Bottom)) ||
